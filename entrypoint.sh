02c84baa6f0d2bff711340d6cfa84c554bb1f427
Nic Cheneweth <nchenewe@thoughtworks.com>
2020-05-24 23:15:20 -0500

    updateing tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
   #!/bin/ash
 inspec exec ./profiles/cis-docker-image --chef-license=accept-no-persist
-
+echo $IMAGE_NAME
+echo $IMAGE_TAG
+echo $CONTAINER_USER
 if [[ $1 != 'distroless' ]]; then
   inspec exec ./profiles/cis-docker-container -t docker://"$CID" --chef-license=accept-no-persist
 fi
